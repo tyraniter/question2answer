220989e7b6ca40a60a8520ca817b5dc67da6c8be
tyraniter <tyraniter@gmail.com>
2018-02-17 22:55:03 +0800

    modify

---
+++ qa-plugin/poll/qa-poll-lang-default.php
@@ -0,0 +1,96 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/example-page/qa-example-lang-default.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: US English language phrases for example plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+/*
+	return array(
+		'permit_post_poll' => 'Create polls',
+		'permit_vote_poll' => 'Vote on polls',
+		'permit_close_poll' => 'Close any poll',
+		'permit_delete_poll' => 'Delete any poll',
+		'permit_delete_poll' => 'Delete any poll',
+		
+		'reopen_poll' => 'Reopen',
+		'reopen_poll_hover' => 'Reopen poll',
+		'close_poll' => 'Close',
+		'close_poll_hover' => 'Close poll',
+		'delete_poll' => 'Delete',
+		'delete_poll_hover' => 'Delete poll',
+		
+		'poll_closed' => 'Poll closed',
+
+		'question_title' => '[poll]',
+		'comments' => '[poll]',
+		'checkbox_text' => 'Create poll',
+		'multiple_text' => 'Allow multiple votes',
+		'vote_button' => 'vote',
+		'voted_button' => 'unvote',
+		'cannot_change' => 'You cannot change your vote once you have voted.',
+		'already_voted' => 'You have already voted once.',
+		'disabled_button' => 'you are not allowed to vote',
+		'answers_text' => 'Choices:',
+		'page_title' => 'Polls',
+		'choice_count_error' => 'You must enter at least two choices for the poll.',
+
+	);
+*/
+
+	return array(
+        'permit_post_poll' => '创建投票',
+        'permit_vote_poll' => '进行投票',
+        'permit_close_poll' => '关闭所有投票',
+        'permit_delete_poll' => '删除所有投票',
+        'permit_delete_poll' => '删除所有投票',
+
+        'reopen_poll' => '重新打开',
+        'reopen_poll_hover' => '重新打开投票',
+        'close_poll' => '关闭',
+        'close_poll_hover' => '关闭投票',
+        'delete_poll' => '删除',
+        'delete_poll_hover' => '删除投票',
+
+        'poll_closed' => '投票已关闭',
+
+        'question_title' => '[投票]',
+        'comments' => '[投票]',
+        'checkbox_text' => '创建投票',
+        'multiple_text' => '允许多选',
+        'vote_button' => '投票',
+        'voted_button' => '已投票',
+        'cannot_change' => '无法修改您的投票',
+        'already_voted' => '您已经投票',
+        'disabled_button' => '您无法投票',
+        'answers_text' => '选项:',
+        'page_title' => '投票',
+        'choice_count_error' => '您必须至少设置两个选项',
+
+    );
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
